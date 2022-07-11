52a437ff01e152e40e3c6db25bd808863adcd8a5
Akshat Sharma <akshatsharma2602@gmail.com>
2022-07-11 17:55:33 +0530

    Made some changes

--- server/server/settings.py
+++ server/server/settings.py
@@ -160,7 +160,6 @@ SWAGGER_SETTINGS = {
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 # Celery Settings
